05f1e6f628475956bb5c0de735529493574e8ee8
Clive D <osde8info@users.noreply.github.com>
2020-12-14 21:59:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const app = express()
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
-        <h1>Hello from ZEIT NOW path '/' on Now 2.0!</h1>
-        <h2>Go to <a href="/about">/about</a></h2>
+        <h1>where can we go today</h1>
+
+        <h2>Surrey</h2>
+        <h3>visit <a href="https://www.visitsurrey.com/">surrey</a></h3>
+        <h3>visit <a href="https://www.visitsurrey.com/explore/towns">surrey map</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/events/whats-on-in-surrey">surrey whats on</a></h3>
+
+        <h2>Sussex</h2>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex">sussex</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex/map">sussex map</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/events/whats-on-in-sussex">sussex whats on</a></h3>
     `)
 })
+
+module.exports = app
 
 if (process.env.DEBUG) app.listen(3000)
